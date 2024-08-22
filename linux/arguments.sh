c71432562aa7b33b0968f778833b0baf6850f57d
2noScript <thangmk7@gmail.com>
2024-08-22 19:22:36 +0700

    chore: show example arguments

--- linux/arguments.sh
+++ linux/arguments.sh
@@ -10,4 +10,37 @@
 # $-	Current options
 # $_	Last argument of the previous command
 
-echo $_
+
+
+# Print the name of the script
+echo "Name of the script: $0"
+
+# Print parameters from $1 to $9 (if available)
+for i in {1..9}; do
+  eval "echo Parameter $i: \$$i"
+done
+
+# Print the 10th parameter (if available)
+if [ $# -ge 10 ]; then
+  echo "Parameter 10: ${10}"
+fi
+
+# Print the number of parameters
+echo "Number of parameters: $#"
+
+# Print the Process ID of the current shell
+echo "Process ID of the current shell: $$"
+
+# Print all parameters as a single string
+echo "All parameters (as a single string): $*"
+
+# Print all parameters, with each parameter as a separate string
+echo "All parameters (each parameter as a separate string): $@"
+
+# Print the current shell options
+echo "Current shell options: $-"
+
+# Print the last argument of the previous command
+echo "Last argument of the previous command: $_"
+
+
---
+++ linux/readme.md
@@ -0,0 +1,5 @@
+- ### Arguments
+
+  ```shell
+  ./arguments.sh a b c d e f g h i j k
+  ```
